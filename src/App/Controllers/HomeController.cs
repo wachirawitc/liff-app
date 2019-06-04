e69b1ea18b06bb2782db9695cc7678945ca70130
Wachirawit Chankaew <wachirawit.chankaew@gmail.com>
2019-06-04 20:48:18 +0700

    Code maid

--- src/App/Controllers/HomeController.cs
+++ src/App/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using App.Models;
 using Microsoft.AspNetCore.Mvc;
-using App.Models;
+using System.Diagnostics;
 
 namespace App.Controllers
 {
