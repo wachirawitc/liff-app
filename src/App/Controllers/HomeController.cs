fda7b6b81339a5898e41467acd970740f51cab08
Wachirawit Chankaew <wachirawit.chankaew@gmail.com>
2019-06-05 05:48:43 +0700

    Init LIFF.

--- src/App/Controllers/HomeController.cs
+++ src/App/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-﻿using App.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace App.Controllers
 {
@@ -9,17 +7,6 @@ namespace App.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
